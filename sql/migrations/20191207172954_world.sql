6b95b7127b235a8735826fc4a8272abce38f7a14
def <defsky@qq.com>
2019-12-08 11:35:23 +0800

    world db migration: Ashbringer event locale

--- sql/migrations/20191207172954_world.sql
+++ sql/migrations/20191207172954_world.sql
@@ -9,7 +9,7 @@ INSERT INTO `migrations` VALUES ('20191207172954');
 -- Start of migration.
 -- Ashbringer event script_texts locale
 
-DELETE FROM `script_texts` WHERE `entry` in (-1999926,-1999925,-1999924,-1999923,-1999922,-1999921,-1999920,-1999919,-1999918)
+DELETE FROM `script_texts` WHERE `entry` in (-1999926,-1999925,-1999924,-1999923,-1999922,-1999921,-1999920,-1999919,-1999918);
 
 INSERT INTO `script_texts` VALUES (-1999926, 'Bow down! Kneel before the Ashbringer! A new dawn approaches, brothers and sisters! Our message will be delivered to the filth of this world through the chosen one!', NULL, NULL, NULL, '叩首！灰烬使者面前下跪！新的黎明即将来临，兄弟姐妹们！我们的信息将通过被选中的人传递给这个世界的污秽！', NULL, NULL, NULL, NULL, 0, 6, 0, 0, 'Ashbringer event');
 INSERT INTO `script_texts` VALUES (-1999925, 'You are forgiven...', NULL, NULL, NULL, '我已经原谅你了……', NULL, NULL, NULL, NULL, 0, 0, 0, 0, 'Ashbringer event');
